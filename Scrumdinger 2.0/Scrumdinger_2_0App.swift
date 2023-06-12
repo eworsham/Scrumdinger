c707f10dc3dc0cfe88a34fe9458a7801f6bcfade
Ryan Worsham <eworsham64@gmail.com>
2023-06-12 15:48:51 -0400

    Initial Commit

---
+++ Scrumdinger 2.0/Scrumdinger_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_2_0App.swift
+//  Scrumdinger 2.0
+//
+//  Created by Ryan Worsham on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
